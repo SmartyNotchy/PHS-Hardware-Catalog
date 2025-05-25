e40391ec68894aab06131734e78d8759bef58230
Coolpie2134 <mktruong09@gmail.com>
2025-05-25 17:48:52 -0400

    added groups page and reformatted css files

---
+++ groupspage.js
@@ -0,0 +1,12 @@
+function arrangeBoxes() {
+  const container = document.querySelector('.main-content');
+  const boxes = container.children;
+  const numBoxes = boxes.length;
+
+  const columns = Math.ceil(Math.sqrt(numBoxes));
+  container.style.gridTemplateColumns = `repeat(${columns}, 1fr)`;
+}
+
+
+
+arrangeBoxes();
