e72bb1ac527408526b37ec7ce8620161484f869e
khang truong <82617978+Coolpie2134@users.noreply.github.com>
2025-06-02 09:11:41 -0400

    back button test

--- about/about.js
+++ about/about.js
@@ -1,9 +1,4 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const backButton = document.getElementById("back");
+function backnavigation(){
+  window.history.back();
+}
 
-  if (backButton) {
-    backButton.addEventListener("click", function () {
-      window.history.back();
-    });
-  }
-});
